2115fd0d5bae7a80c2f5a8e47e3a7905c355cf21
unknown <yashkhandelwal003@gmail.com>
2021-08-15 10:05:29 +0530

    added link to default page

--- src/Pages/PageNotFound.js
+++ src/Pages/PageNotFound.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const PageNotFound = () => {
     return (
         <div>
+            <Link to="/signin"><h1 className="welcome-page"> Welcome to Git App </h1></Link>
         </div>
     );
 };
